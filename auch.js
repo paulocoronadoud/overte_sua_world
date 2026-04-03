c2a4f72203e3d7ee3da9b42face40d3e14db8cf9
Paulo César Coronado <108105700+paulocoronadoud@users.noreply.github.com>
2026-04-03 05:22:46 -0500

    Update auch.js

--- auch.js
+++ auch.js
@@ -1,20 +1,12 @@
 (function () {
     return {
         preload: function (entityID) {
-            this.entityID = entityID;
-            print("[AUCH] Script cargado: " + entityID);
+            print("[AUCH] Script cargado");
         },
 
         collisionWithEntity: function (thisEntityID, otherEntityID, collision) {
-            print("[AUCH] collisionWithEntity disparado");
-            print("[AUCH] thisEntityID: " + thisEntityID);
-            print("[AUCH] otherEntityID: " + otherEntityID);
-            print("[AUCH] MyAvatar.sessionUUID: " + MyAvatar.sessionUUID);
-
-            if (otherEntityID === MyAvatar.sessionUUID) {
-                print("[AUCH] Colisión con mi avatar");
-                Messages.sendMessage("Chat", "auch");
-            }
+            print("[AUCH] collisionWithEntity disparado con: " + otherEntityID);
+            Messages.sendMessage("Chat", "auch");
         }
     };
 });
