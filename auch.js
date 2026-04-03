dc8d436aa72a287ac953e6eca1dfdeb6e31e2bae
Paulo César Coronado <108105700+paulocoronadoud@users.noreply.github.com>
2026-04-03 05:53:01 -0500

    Update auch.js

--- auch.js
+++ auch.js
@@ -1,25 +1,16 @@
 (function () {
-    var _entityID = null;
-
-    function onCollision(entityA, entityB, collision) {
-        print("[AUCH] El cubo detectó una colisión");
-        print("[AUCH] entityA = " + entityA);
-        print("[AUCH] entityB = " + entityB);
-    }
-
     return {
         preload: function (entityID) {
-            _entityID = entityID;
+            this.entityID = entityID;
             print("[AUCH] Script cargado en cubo: " + entityID);
-
-            Script.addEventHandler(entityID, "collisionWithEntity", onCollision);
         },
 
-        unload: function () {
-            if (_entityID) {
-                Script.removeEventHandler(_entityID, "collisionWithEntity", onCollision);
-            }
-            print("[AUCH] Script descargado");
+        enterEntity: function (entityID) {
+            print("[AUCH] El avatar tocó el cubo");
+        },
+
+        leaveEntity: function (entityID) {
+            print("[AUCH] El avatar salió del cubo");
         }
     };
 });
