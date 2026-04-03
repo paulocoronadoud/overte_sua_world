d926434ec4e0b8e0e786a3b2ac862e24f2c6e185
Paulo César Coronado <108105700+paulocoronadoud@users.noreply.github.com>
2026-04-03 05:48:39 -0500

    Update auch.js

--- auch.js
+++ auch.js
@@ -1,12 +1,25 @@
-(function() {
-    // Esta función se ejecuta automáticamente cuando un avatar toca la entidad
-    this.collisionWithAvatar = function(entityID, avatarID, collision) {
-        
-        // 'collision' contiene datos del impacto (fuerza, punto de contacto, etc.)
-        // Pero para tu requerimiento, solo imprimiremos el mensaje:
-        print("me empujaron");
-        
-        // Opcional: Si quieres saber quién fue:
-        // print("Me empujó el avatar con ID: " + avatarID);
+(function () {
+    var _entityID = null;
+
+    function onCollision(entityA, entityB, collision) {
+        print("[AUCH] El cubo detectó una colisión");
+        print("[AUCH] entityA = " + entityA);
+        print("[AUCH] entityB = " + entityB);
+    }
+
+    return {
+        preload: function (entityID) {
+            _entityID = entityID;
+            print("[AUCH] Script cargado en cubo: " + entityID);
+
+            Script.addEventHandler(entityID, "collisionWithEntity", onCollision);
+        },
+
+        unload: function () {
+            if (_entityID) {
+                Script.removeEventHandler(_entityID, "collisionWithEntity", onCollision);
+            }
+            print("[AUCH] Script descargado");
+        }
     };
 });
