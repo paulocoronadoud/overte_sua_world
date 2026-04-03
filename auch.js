b2776242d039679b90408578c7248574664f1bbd
Paulo César Coronado <108105700+paulocoronadoud@users.noreply.github.com>
2026-04-03 05:08:17 -0500

    Update auch.js

--- auch.js
+++ auch.js
@@ -1,10 +1,21 @@
 (function () {
     return {
         preload: function (entityID) {
-            print("[AUCH] Script cargado. entityID = " + entityID);
+            this.entityID = entityID;
+            print("[AUCH] Script cargado para entidad: " + entityID);
         },
+
         unload: function () {
-            print("[AUCH] Script descargado.");
+            print("[AUCH] Script descargado");
+        },
+
+        collisionWithEntity: function (thisEntityID, otherEntityID, collision) {
+            print("[AUCH] Colisión detectada con: " + otherEntityID);
+
+            if (otherEntityID === MyAvatar.sessionUUID) {
+                print("[AUCH] El avatar local colisionó");
+                Messages.sendMessage("Chat", "auch");
+            }
         }
     };
 });
