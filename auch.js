a96d996137f62be16c4c231bb93fdcbfc4c12cde
Paulo César Coronado <108105700+paulocoronadoud@users.noreply.github.com>
2026-04-03 06:00:18 -0500

    Update auch.js

--- auch.js
+++ auch.js
@@ -1,16 +1,17 @@
-(function () {
-    return {
-        preload: function (entityID) {
-            this.entityID = entityID;
-            print("[AUCH] Script cargado en cubo: " + entityID);
-        },
+(function() {
+    this.preload = function(entityID) {
+        print("DEBUG: Script cargado. Esperando colisiones en: " + entityID);
+    };
 
-        enterEntity: function (entityID) {
-            print("[AUCH] El avatar tocó el cubo");
-        },
-
-        leaveEntity: function (entityID) {
-            print("[AUCH] El avatar salió del cubo");
-        }
+    // Este es el nombre real del método en el API de Overte
+    this.collisionWithEntity = function(myID, otherID, collision) {
+        
+        // El motor nos da el ID de lo que nos golpeó (otherID)
+        // Podemos verificar si ese ID pertenece a un avatar
+        var properties = Entities.getEntityProperties(otherID, ["type"]);
+        
+        // Si el 'otherID' no devuelve propiedades de entidad, 
+        // es muy probable que sea un Avatar (o podemos usar AvatarList)
+        print("¡MENSAJE: Me empujaron! ID del agresor: " + otherID);
     };
 });
